1eab998f81b0b884773f1aa15ea8430c7ff0c697
Mutesi <ktesialine@gmail.com>
2022-11-09 15:13:17 +0200

    bjhb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+  content: [ 
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
   ],
